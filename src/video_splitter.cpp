e1cee9ebf62d6f6e3471403a5b8da061cd5564ff
Kyle Goehner <kylea.goehner@gmail.com>
2014-10-04 11:24:48 -0500

    Add input parameters and drawing rectangle

--- src/video_splitter.cpp
+++ src/video_splitter.cpp
@@ -5,26 +5,53 @@
 
 #include <glog/logging.h>
 
+#define VISUAL
+
 // Name of the main program window
 static const std::string W_NAME = "WINDOW";
 
 // Frequency in seconds to write frame to disk
 size_t WRITE_FREQUENCY = 5;
 
+std::string getUsage() {
+    std::stringstream ss;
+    ss << "Usage: <video> <x position> <y position> <scale size>";
+    return ss.str();
+}
+
+std::string getDesc() {
+    std::stringstream ss;
+    ss << "Description: All inputs should be normalized: [0, 1]";
+    return ss.str();
+}
+
 int main(int argc, char** argv) {
-    // Initiate Google Logging
-    google::InitGoogleLogging(argv[0]);
     // Log to stderr instead of to the tmp directory
     FLAGS_logtostderr = 1;
+    // Initiate Google Logging
+    google::InitGoogleLogging(argv[0]);
 
+    if (argc < 5) {
+        LOG(ERROR) << "Not enough arguments:";
+        LOG(ERROR) << getUsage();
+        LOG(ERROR) << getDesc();
+        exit(1);
+    }
+
+    // Read input values
     std::string filename = argv[1];
+    float x_val = atof(argv[2]);
+    float y_val = atof(argv[3]);
+    float size_val = atof(argv[4]);
+
     LOG(INFO) << "Opening video: '" << filename << "'";
     cv::VideoCapture capture(filename);
     cv::Mat frame;
 
     if (!capture.isOpened()) {
-        //TODO Log error!
-        throw "Error when reading stream";
+        std::string error_message = "Error when reading input stream";
+        LOG(FATAL) << error_message;
+        throw error_message;
     }
 
     //cv::namedWindow(W_NAME, cv::WINDOW_AUTOSIZE);
@@ -32,13 +59,14 @@ int main(int argc, char** argv) {
     size_t frame_num = capture.get(CV_CAP_PROP_POS_FRAMES);
     int frame_width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
     int frame_height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
+    float diameter = sqrt(frame_width * frame_width + frame_height * frame_height);
 
-    cv::Point2i top_left(frame_width * 0.45, frame_height * 0.45);
-    cv::Point2i bottom_right(frame_width * 0.55, frame_height * 0.55);
-    LOG(INFO) << "TL: " << top_left;
-    LOG(INFO) << "BR: " << bottom_right;
-    cv::Rect roi(top_left, bottom_right);
-    LOG(INFO) << "RoI: " << roi;
+    cv::Point2i top_left(frame_width * x_val, frame_height * y_val);
+    cv::Size2i size(diameter * size_val, diameter * size_val);
+    LOG(INFO) << "Top Left: \t" << top_left;
+    LOG(INFO) << "Size: \t" << size;
+    cv::Rect roi(top_left, size);
+    LOG(INFO) << "RoI: \t" << roi;
 
     while (true) {
         frame_num += WRITE_FREQUENCY * frame_rate;
@@ -50,18 +78,20 @@ int main(int argc, char** argv) {
             break;
         }
 
-        //if (frame_num % (WRITE_FREQUENCY * frame_rate) == 0) {
+#ifdef VISUAL
+        cv::rectangle(frame, roi, cv::Scalar(0, 0, 255));
+        cv::imshow(W_NAME, frame);
+        //cv::waitKey(static_cast<int64_t>(1000 * 1/frame_rate)); // Draw at correct frame rate.
+        cv::waitKey(1); // Draw as fast as possible.
+#endif
+
         std::stringstream ss;
         ss << "./" << frame_num << ".jpg";
         cv::imwrite(ss.str(), frame(roi));
-        //}
-
-        //cv::imshow(W_NAME, frame);
-        //cv::waitKey(static_cast<int64_t>(1000 * 1/frame_rate)); // Draw at correct frame rate.
-        //cv::waitKey(1); // Draw as fast as possible.
     }
 
+#ifdef VISUAL
     cv::destroyWindow(W_NAME);
-    LOG(INFO) << "::: EXIT :::";
+#endif
     return 0;
 }

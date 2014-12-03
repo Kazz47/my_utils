9db5e9b72964448d2829da4c5940b416942a71c4
Kyle Goehner <KyleA.Goehner@gmail.com>
2014-12-02 19:24:42 -0600

    Added mask to remove roads and dirt patches

--- src/blob_count.cpp
+++ src/blob_count.cpp
@@ -47,22 +47,24 @@ int main(int argc, char** argv) {
 
     // Blob Detection Params
     cv::SimpleBlobDetector::Params params;
-    params.minDistBetweenBlobs = 100.0f;
+    params.minDistBetweenBlobs = 10.0f;
     params.filterByInertia = false;
     params.filterByConvexity = false;
     params.filterByColor = true;
     params.blobColor = 255;
-    params.filterByCircularity = false;
+    params.filterByCircularity = true;
+    params.minCircularity = 0.1; //0.1
+    params.maxCircularity = 0.9; //0.9
     params.filterByArea = true;
-    params.minArea = min_area; //600
-    params.maxArea = max_area; //7200
+    params.minArea = min_area; //450
+    params.maxArea = max_area; //9500
 
     cv::Ptr<cv::FeatureDetector> blob_detect = new cv::SimpleBlobDetector(params);
     blob_detect->create("BisonBlob");
 
     LOG(INFO) << "Checking for blobs in: '" << img_filename << "'";
     cv::Mat image = cv::imread(img_filename, CV_LOAD_IMAGE_COLOR);
-    //cv::resize(image, image, cv::Size(2056,1028));
+    //cv::GaussianBlur(image, image, cv::Size(0, 0), 5);
 
     if (image.empty()) {
         LOG(FATAL) << "Image failed to load...";
@@ -70,6 +72,9 @@ int main(int argc, char** argv) {
 
 #ifdef VISUAL
     cv::namedWindow(W_NAME, cv::WINDOW_NORMAL);
+    //cv::namedWindow("Hue", cv::WINDOW_NORMAL);
+    //cv::namedWindow("Sat", cv::WINDOW_NORMAL);
+    //cv::namedWindow("Val", cv::WINDOW_NORMAL);
     //cv::imshow(W_NAME, image);
     //cv::waitKey(5000); // Wait for 5 seconds
 #endif
@@ -85,23 +90,35 @@ int main(int argc, char** argv) {
     std::vector<cv::Mat> channels;
     cv::split(hsv, channels);
     cv::Mat hue = channels[0];
+    cv::Mat sat = channels[1];
+    cv::Mat val = channels[2];
+#ifdef VISUAL
+    //cv::imshow("Hue", hue);
+    //cv::imshow("Sat", sat);
+    //cv::imshow("Val", val);
+    //cv::waitKey(5000); // Wait for 5 seconds
+#endif
+    cv::Mat mask = cv::Mat::zeros(sat.size(), sat.type());
+    mask = sat > 20;
     cv::Mat thresh;
     cv::threshold(hue, thresh, 22, 255, cv::THRESH_BINARY_INV);
+    thresh = thresh & mask;
     std::vector<cv::KeyPoint> keypoints;
     blob_detect->detect(thresh, keypoints);
     LOG(INFO) << "Bison found: " << keypoints.size();
 
 #ifdef VISUAL
+    cv::imshow(W_NAME, mask);
     //cv::imshow(W_NAME, thresh);
-    //cv::waitKey(5000); // Wait for 5 seconds
+    cv::waitKey(5000); // Wait for 5 seconds
 #endif
 
     cv::Mat hsv_keypoints;
-    drawKeypoints(thresh, keypoints, hsv_keypoints, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
+    drawKeypoints(thresh, keypoints, hsv_keypoints, cv::Scalar(255, 0, 0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
 #ifdef VISUAL
     cv::imshow(W_NAME, hsv_keypoints);
-    cv::waitKey(10000); // Wait for 5 seconds
+    cv::waitKey(); // Wait for 5 seconds
 #endif
 
 #ifdef VISUAL
